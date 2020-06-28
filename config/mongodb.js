c5b8109c2d9c66beb50854d288211e37d9715eea
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-06-28 14:13:21 -0300

    Fixed mongo connection constuctor

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,6 @@
 const { mongoDb } = require('../env-select')
 const mongoose = require('mongoose')
-mongoose.connect(mongoDb.connection, { useNewUrlParser: true })
+mongoose.connect(mongoDb.connection, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(e => {
         const msg = 'ERROR: Cannot connect to MondoDB!'
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
